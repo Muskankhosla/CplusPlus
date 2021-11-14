1071f3d97da763db15aa6155389edf6238357007
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-14 12:33:36 +0530

    Create 2.2.cpp

---
+++ C++ Self Practice/Chapter 2/2.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{   int a,b;
+ cin>>a>>b;
+ if(a>b)
+ {
+     cout<<a;
+ }
+ else
+ cout<<b;
+
+
+    return 0;
+}
+
+
+
+
+i/p : 12 78
+  o/p: 78
