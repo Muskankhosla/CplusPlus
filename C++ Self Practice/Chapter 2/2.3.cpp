a64e64de286d2f2f2d2b69cc86b9ae8765f87323
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-14 12:35:52 +0530

    Create 2.3.cpp

---
+++ C++ Self Practice/Chapter 2/2.3.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{  int a,i;
+cin>>a;
+
+for(i=1;i<=a;i++)
+
+{
+    cout<<"Well Done\n";
+}
+
+    return 0;
+}
+
+
+
+i/p: 5
+  o/p:
+Well Done
+Well Done
+Well Done
+Well Done
+Well Done
