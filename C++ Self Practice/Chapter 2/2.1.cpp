26f149a74dbcdd6f68df7fc5ac8395dd10acc319
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-14 12:31:55 +0530

    Create 2.1.cpp

---
+++ C++ Self Practice/Chapter 2/2.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{   int maths,physics,chem;
+   cin>>maths>>physics>>chem;
+   cout<<maths<<" "<<physics<<" "<<chem;
+
+
+    return 0;
+}
+  input:  
+99
+77
+69
+  output:
+99 77 69
