b55f8547e48b01f12dd2ea70d73496d0ab65431b
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-14 13:20:22 +0530

    Create 3.4.cpp

---
+++ C++ Self Practice/Chapter 3/3.4.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    float p,v,r,n;
+    cin>>p>>r>>n;
+    v=p*pow((1+r),n);
+    cout<<v;
+
+    return 0;
+}
+
+
+
+1000
+0.10
+1
+1100
