8092305332f39c72e6e344a327c8282740aeb1a1
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:46:46 +0530

    Create Vowel or not.cpp

---
+++ Codes/Vowel or not.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{  char ch;
+cin>>ch;
+switch(ch)
+{
+    case 'a':
+       cout<<"Vowel";
+       break;
+       
+         case 'e':
+       cout<<"Vowel";
+       break;
+       
+         case 'i':
+       cout<<"Vowel";
+       break;
+       
+         case 'o':
+       cout<<"Vowel";
+       break;
+       
+         case 'u':
+       cout<<"Vowel";
+       break;
+       default:
+       cout<<"Consonant";
+}
+    return 0;
+}
+e
+Vowel
