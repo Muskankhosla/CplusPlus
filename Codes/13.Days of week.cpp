4fdce70e5e7cad964c82402d8ef6c843f56ff293
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:29:11 +0530

    Create 13.Days of week.cpp

---
+++ Codes/13.Days of week.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{ int num;
+cin>>num;
+switch(num)
+{
+    case 1:
+    cout<<"Monday";
+    break;
+     case 2:
+    cout<<"Tuesday";
+    break;
+     case 3:
+    cout<<"Wednesday";
+    break;
+     case 4:
+    cout<<"Thursday";
+    break;
+     case 5:
+    cout<<"Friday";
+    break;
+     case 6:
+    cout<<"Saturday";
+    break;
+     case 7:
+    cout<<"Sunday";
+    break;
+    default:
+    cout<<"Invalid num";
+}
+    return 0;
+}
+8
+Invalid num
