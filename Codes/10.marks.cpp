d23d2eaf5d07b638bbbd8a121d7dc5cff7f48d7c
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:05:18 +0530

    Create 10.marks.cpp

---
+++ Codes/10.marks.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{ int a;
+char grade;
+cin>>a;
+if(a>=90)
+grade='A';
+else if(a>=80)
+grade='B';
+else if(a>=70)
+grade='C';
+else if(a>=60)
+grade='D';
+else
+grade='F';
+cout<<grade;
+    return 0;
+}
+
+25
+F
