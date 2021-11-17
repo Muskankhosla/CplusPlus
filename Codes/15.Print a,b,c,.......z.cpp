c7d23637f7c5af9c387e35ed6ecef64f01383035
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:48:58 +0530

    Create 15.Print a,b,c,.......z.cpp

---
+++ Codes/15.Print a,b,c,.......z.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{  char ch;
+cin>>ch;
+for(ch>='a';ch<='z';ch++)
+{
+    cout<<ch<<" ";
+}
+    return 0;
+}
+
+g
+g h i j k l m n o p q r s t u v w x y z 
