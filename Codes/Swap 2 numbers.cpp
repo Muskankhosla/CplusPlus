d4833604ff06d03cbceb42c9c62796cacd829a85
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 15:19:29 +0530

    Create Swap 2 numbers.cpp

---
+++ Codes/Swap 2 numbers.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{ int a,b,temp;
+cout<<"Before swap :";
+cin>>a>>b;
+ temp=a;
+ a=b;
+ b=temp;
+ cout<<"after swap: "<<a<<" "<<b;
+    return 0;
+}
+Before swap :1 2 
+after swap: 2 1
