85609023cc75babd2a2ba114f5f3e25414042ddb
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:51:26 +0530

    Create 17.Print all even between 1 to 20.cpp

---
+++ Codes/17.Print all even between 1 to 20.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{  int c,i;
+cin>>c;
+for(i=c;i<=20;i++)
+{ if(i%2==0)
+    cout<<i<<" ";
+}
+    return 0;
+}
+
+3
+4 6 8 10 12 14 16 18 20 
