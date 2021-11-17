f69d95b90cf97c1cf5fc1a45f80506cb5d74914d
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-17 16:24:55 +0530

    Create 12.Valid Triangle .cpp

---
+++ Codes/12.Valid Triangle .cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{  float a,b,c;
+cin>>a>>b>>c;
+if(a+b+c==180)
+{
+    cout<<"Valid Triangle";
+    
+}
+else
+cout<<"Invalid Triangle";
+    return 0;
+}
+30 60 90
+Valid Triangle
