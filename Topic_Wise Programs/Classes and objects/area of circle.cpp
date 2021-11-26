3c3feb73bc46c1c5c8fb60fa9756a62769e85b4e
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-26 11:11:27 +0530

    Create area of circle.cpp

---
+++ Topic_Wise Programs/Classes and objects/area of circle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class circle{
+   int radius;
+   public:
+   void getRad(){
+       cout<<"Enter the radius :";
+       cin>>radius;
+   }
+    int area()
+    { int a;
+     return  a=radius*radius;
+    }
+    void disp(){
+        cout<<area();
+    }
+    
+};
+int main()
+{
+   circle ob1;
+   ob1.getRad();
+   ob1.disp();
+
+    return 0;
+}
