14e442338e875ffba467a792da5b5b4565282bff
Muskan Khosla <khosla.muskan01@gmail.com>
2021-11-26 13:46:44 +0530

    Update volume of cube.cpp

--- Topic_Wise Programs/Constructors & destructors/volume of cube.cpp
+++ Topic_Wise Programs/Constructors & destructors/volume of cube.cpp
@@ -1,1 +1,33 @@
-
+#include <iostream>
+#include<string>
+using namespace std;
+class volume{
+    
+    public:
+    int s,vol;
+     volume();
+     void get();
+     void disp();
+     
+};
+volume::volume()
+{ 
+   vol=0;
+}
+void volume::get()
+{
+    cin>>s;
+    
+}
+void volume::disp()
+{  vol=s*s*s;
+    cout<<vol<<endl;
+}
+    int main()
+{
+   
+ volume ob;
+   ob.get();
+   ob.disp();
+   return 0;
+}
